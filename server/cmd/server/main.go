783cdb74203c429dcb41692a96065d85913f65b1
DAN <87706283+ItsMeD4N@users.noreply.github.com>
2025-12-12 13:22:15 +0700

    fix : fix origin

--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -76,7 +76,7 @@ func main() {
 
 	// Setup CORS
 	r.Use(func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", c.Request.Header.Get("Origin"))
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
